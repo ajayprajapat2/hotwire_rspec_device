83b385ebc02d7a130d4920903c2d0e361c813c51
Ajay prajapat <ajay.p@arbutusinfotech.com>
2024-01-22 13:38:45 +0530

    update code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,19 +22,35 @@ Rails.application.configure do
   # Enable/disable caching. By default caching is disabled.
   # Run rails dev:cache to toggle caching.
   if Rails.root.join('tmp/caching-dev.txt').exist?
-    configure_caching(config)
+    config.action_controller.perform_caching = true
+    config.action_controller.enable_fragment_cache_logging = true
+
+    config.cache_store = :memory_store
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_i}"
+    }
   else
-    configure_no_caching(config)
+    config.action_controller.perform_caching = false
+
+    config.cache_store = :null_store
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  configure_mailer(config)
+  config.action_mailer.raise_delivery_errors = false
+
+  config.action_mailer.perform_caching = false
 
   # Print deprecation notices to the Rails logger.
-  configure_deprecation(config)
+  config.active_support.deprecation = :log
+
+  # Raise exceptions for disallowed deprecations.
+  config.active_support.disallowed_deprecation = :raise
+
+  # Tell Active Support which deprecation messages to disallow.
+  config.active_support.disallowed_deprecation_warnings = []
 
   # Raise an error on page load if there are pending migrations.
   config.active_record.migration_error = :page_load
@@ -45,33 +61,12 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  config.action_mailer.delivery_method = :letter_opener
-  config.action_mailer.perform_deliveries = true
-end
+  # Raises error for missing translations.
+  # config.i18n.raise_on_missing_translations = true
 
-def configure_caching(config)
-  config.action_controller.perform_caching = true
-  config.action_controller.enable_fragment_cache_logging = true
+  # Annotate rendered view with file names.
+  # config.action_view.annotate_rendered_view_with_filenames = true
 
-  config.cache_store = :memory_store
-  config.public_file_server.headers = {
-    'Cache-Control' => "public, max-age=#{2.days.to_i}"
-  }
-end
-
-def configure_no_caching(config)
-  config.action_controller.perform_caching = false
-  config.cache_store = :null_store
-end
-
-def configure_mailer(config)
-  config.action_mailer.raise_delivery_errors = false
-  config.action_mailer.perform_caching = false
-end
-
-def configure_deprecation(config)
-  config.active_support.deprecation = :log
-  config.active_support.disallowed_deprecation = :raise
-  config.active_support.disallowed_deprecation_warnings = []
+  # Uncomment if you wish to allow Action Cable access from any origin.
+  # config.action_cable.disable_request_forgery_protection = true
 end
